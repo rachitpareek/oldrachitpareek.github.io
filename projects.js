3949eb62d79dd1f08492dc9a557b459c15ae8be6
Rachit Pareek <pareek.rachit1@gmail.com>
2019-06-11 17:43:48 -0700

    Finally got JS to work!

--- projects.js
+++ projects.js
@@ -1,8 +1,3 @@
-function countToTen(){
- var count = 0;
- while (count < 10) {
- count++;
- document.getElementById(“theCount”).innerHTML += count + “<br>“;
- document.write(count);
- }
+function sendAlert(){
+ alert('This is a file test!');
 }
